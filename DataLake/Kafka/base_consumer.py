38613b89c06d742c95b069ff694dfe6f9c811c64
jinyes-kim <jinung@kakao.com>
2021-09-15 00:58:36 +0900

    Update Base Kafka Producer&Consumer Code - 2021.09.15.

--- DataLake/Kafka/base_consumer.py
+++ DataLake/Kafka/base_consumer.py
@@ -1,1 +1,17 @@
 from kafka import KafkaConsumer
+
+
+broker = ['localhost:9092']
+topic = "news"
+consumer = KafkaConsumer(topic,
+                         group_id="group_1",
+                         bootstrap_servers=broker,
+                         auto_offset_reset="earliest")
+
+try:
+    for msg in consumer:
+        print("Topic: {}\nPartition: {}\nOffset: {}\nKey: {}\nValue: {}\n".format(
+            msg.topic, msg.partition, msg.offset, msg.key, msg.value.decode("utf-8")))
+
+except KeyboardInterrupt:
+    exit(0)
--- DataLake/Kafka/base_producer.py
+++ DataLake/Kafka/base_producer.py
@@ -1,1 +1,10 @@
 from kafka import KafkaProducer
+
+broker = ["localhost:9092"]
+topic = "news"
+producer = KafkaProducer(bootstrap_servers=broker)
+
+# Send test message
+msg = "Hello Kafka"
+producer.send(topic, msg.encode("utf-8"))
+producer.flush()
