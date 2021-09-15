a6085e84dbe79b39db48a1ede435d95c0f883e97
jinyes-kim <jinung@kakao.com>
2021-09-15 19:21:03 +0900

    기본 예외처리 추가 - 2021.09.15.
    예외처리 발생한 부분을 slack으로 전송하는 로직 추가하기

--- News-Crawler/Naver/main.py
+++ News-Crawler/Naver/main.py
@@ -13,11 +13,16 @@ def main():
     idx = 0
     for subject in subject_list:
         specific_subject_list = file["specific_keywords"][subject]
+
         for specific_subject in specific_subject_list:
             category1 = file["keyword_code"][subject]
             category2 = file["specific_keyword_code"][specific_subject]
-            raw = extract_article(date=today, category1=category1, category2=category2)
-            result = preprocess_article(raw)
+            try:
+                raw = extract_article(date=today, category1=category1, category2=category2)
+                result = preprocess_article(raw)
+            except Exception as Error:
+                # slack으로 에러 발생한 지점 전송
+                continue
             idx += len(result)
             pprint(result)
 
