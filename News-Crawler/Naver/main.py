a85cfbefe996560b1a57bb673dbe78e25f62b1b5
jinyes-kim <jinung@kakao.com>
2021-09-17 15:13:54 +0900

    디렉토리 이름 변경 - 2021.09.17.

--- News-Crawler/Naver/main.py
+++ News-Crawler/Naver/main.py
@@ -1,34 +1,34 @@
-from naver_crawler import *
+from naver_news.news_crawler import *
 from datetime import datetime
-from pprint import pprint
+from kafka import KafkaProducer
 import json
 import time
 
 
 def main():
     today = datetime.now().strftime("%Y%m%d")
-    file = json.load(open("category.json", 'r'))
+    file = json.load(open("./data/category.json", 'r'))
     subject_list = file["keywords"]
 
-    idx = 0
     for subject in subject_list:
         specific_subject_list = file["specific_keywords"][subject]
-
         for specific_subject in specific_subject_list:
             category1 = file["keyword_code"][subject]
             category2 = file["specific_keyword_code"][specific_subject]
             try:
                 raw = extract_article(date=today, category1=category1, category2=category2)
-                result = preprocess_article(raw)
+                news_list = preprocess_article(raw)
             except Exception as Error:
                 # slack으로 에러 발생한 지점 전송
+                print(Error)
                 continue
-            idx += len(result)
-            pprint(result)
 
-            time.sleep(5)
+            for news in news_list:
+                data = ','.join([today, subject, specific_subject, news[0], news[1]])
+                print(data)
+                # data로 카프카에 전송하기
 
-    print(idx)
+            time.sleep(2)
 
 
 if __name__ == "__main__":
--- News-Crawler/Naver/naver_news/news_crawler.py
+++ News-Crawler/Naver/naver_news/news_crawler.py
@@ -5,23 +5,23 @@ from bs4 import BeautifulSoup
 
 def extract_article(date, category1, category2):
     result = []
-    # 최대 페이지 체크
     url = "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1={}&sid2={}".format(category1, category2)
     request = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     soup = BeautifulSoup(request.text, "html.parser")
-    limit = soup.select(".paging > a")
+    last_page = soup.select(".paging > a")
     target = (".type06_headline > li > dl > dt:nth-child(1) > a",
               ".type06 > li > dl > dt:nth-child(1) > a")
 
     # 섹션 별 데이터 수집
-    for i in range(1, len(limit)+2):
+    for i in range(1, len(last_page)+2):
         url = "https://news.naver.com/main/list.naver?mode=LS2D&sid2={1}&sid1={0}&mid=shm&date={2}&page={3}".\
             format(category1, category2, date, i)
         request = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
         soup = BeautifulSoup(request.content.decode("euc-kr", "replace"), "html.parser")
+
         for section in target:
             result.extend(soup.select(section))
-        time.sleep(2) # delay
+        time.sleep(2)
 
     return result
 
