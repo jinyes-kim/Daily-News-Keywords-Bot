389a3c1768138be022208c803184ace0c42bf82f
jinyes-kim <jinung@kakao.com>
2021-09-23 17:40:13 +0900

    명사 추출 스크립트 개발 - 2021.09.23.
    Okt가 가장 요구하는 목적에 부합한 결과를 보여주고 있음
    추출한 명사 데이터를 엘라스틱 서치에 어떤 방식으로 저장할 것인지
    스키마 고려하기

--- DataLake/Data-Processing/main.py
+++ DataLake/Data-Processing/main.py
@@ -1,18 +1,33 @@
+from konlpy.tag import Okt
 from pymongo import MongoClient
 from datetime import datetime
 
-pw = open("/Users/jinyes/git/Daily-News-Keywords-Bot/pw.txt", 'r').read()
 today = datetime.now().strftime("%Y%m%d")
+pw = open("/Users/jinyes/git/Daily-News-Keywords-Bot/pw.txt", 'r').read()
 client = MongoClient(host="jinyes-server",
                      port=27017,
                      username="root",
                      password=pw)
-
 db = client["news"]
 collection = db["news_test"]
+
+# query
 records = collection.find({"date": str(today)})
 
+jvm_path = "/Library/Java/JavaVirtualMachines/zulu-15.jdk/Contents/Home/bin/java"
+okt = Okt(jvmpath=jvm_path)
 
-for record in records:
-    print(record)
-    break
+for idx, record in enumerate(records):
+    subject = record["subject"]
+    specific_subject = record["specific_subject"]
+    title = record["title"]
+    noun = okt.nouns(title)
+    print(subject, specific_subject, title, noun)
+
+    """
+    엘라스틱 서치의 도큐먼트 큰 범주에 단어를 삽입하는 방식으로 진행
+    
+    """
+
+    if idx == 100:
+        break
