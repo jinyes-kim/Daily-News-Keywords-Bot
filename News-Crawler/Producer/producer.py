a00080a35bd72f1a14bf2d3196e0513a1f85c8ff
jinyes-kim <jinung@kakao.com>
2021-09-23 16:50:51 +0900

    디비 접근 방식 변경 - 2021.09.23.

--- News-Crawler/Producer/producer.py
+++ News-Crawler/Producer/producer.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from datetime import datetime
-from password import pw
 
+pw = open("/Users/jinyes/git/Daily-News-Keywords-Bot/pw.txt", 'r').read()
 client = MongoClient(host="jinyes-server",
                      port=27017,
                      username="jinyes",
