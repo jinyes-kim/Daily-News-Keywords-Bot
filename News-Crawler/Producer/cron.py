37960349a459506e746e1f322a308cf40ee78cb2
jinyes-kim <jinung@kakao.com>
2021-09-23 16:02:10 +0900

    배치 데이터 삽입 스크립트 작성 - 2021.09.23.

---
+++ News-Crawler/Producer/cron.py
@@ -0,0 +1,38 @@
+from pymongo import MongoClient
+from datetime import datetime
+from password import pw
+
+client = MongoClient(host="jinyes-server",
+                     port=27017,
+                     username="jinyes",
+                     password=pw)
+
+db = client.news
+
+
+def to_bson(data):
+    raw = data.split(",")
+    doc = {
+        "date": raw[0],
+        "subject": raw[1].strip(),
+        "specific_subject": raw[2].strip(),
+        "title": raw[3].strip(),
+        "url": raw[4].strip(),
+    }
+
+    return doc
+
+
+def main():
+    today = datetime.now().strftime("%Y%m%d")
+    dataset = []
+    with open("/Users/jinyes/git/Daily-News-Keywords-Bot/Data/{}.txt".format(today), 'r') as records:
+        for record in records:
+            doc = to_bson(record)
+            dataset.append(doc)
+
+    db.news_test.insert_many(dataset)
+
+
+if __name__ == "__main__":
+    main()
