cbd2813e2723546903178dfe6b36dabf8ec65e8a
jinyes-kim <jinung@kakao.com>
2021-09-30 14:37:08 +0900

    키워드 및 세부 키워드 적용 - 2021.09.24.
    [기존]
    2개의 케이스로만 테스트
    
    [변경]
    모든 뉴스 키워드 데이터 기반으로 카운팅

--- Bot/main.py
+++ Bot/main.py
@@ -1,38 +1,43 @@
 from library import slack
 from library.es_query import *
 from collections import defaultdict
+import json
 
 
 def main():
-    for specific_subject in ["청와대", "사회일반"]:
-        # Read Data
-        records = request_data("20210924", specific_subject)
+    with open("/Users/jinyes/git/Daily-News-Keywords-Bot/Crawler/Naver/info/category.json") as catregory:
+        file = json.load(catregory)
 
-        # Count Nouns
-        noun_dict = defaultdict(int)
-        for record in records["hits"]["hits"]:
-            noun_list = record["_source"]["title_noun"]
-            for noun in noun_list:
-                noun_dict[noun] += 1
+    for subject in file["keywords"]:
+        for specific_subject in file["specific_keywords"][subject]:
+            # Read Data
+            records = request_data("20210924", specific_subject)
 
-        rank_list = []
-        for noun, cnt in noun_dict.items():
-            rank_list.append([noun, cnt])
+            # Count Nouns
+            noun_dict = defaultdict(int)
+            for record in records["hits"]["hits"]:
+                noun_list = record["_source"]["title_noun"]
+                for noun in noun_list:
+                    noun_dict[noun] += 1
 
-        # Sort
-        rank_list.sort(key=lambda x: x[1], reverse=True)
-        limit = rank_list[2][1]
+            rank_list = []
+            for noun, cnt in noun_dict.items():
+                rank_list.append([noun, cnt])
 
-        # Result
-        msg = "[{}]\n".format(specific_subject)
-        for keyword, cnt in rank_list:
-            if cnt >= limit:
-                msg += "{}: {}회 등장\n".format(keyword, cnt)
-            else:
-                break
+            # Sort
+            rank_list.sort(key=lambda x: x[1], reverse=True)
+            limit = rank_list[2][1]
 
-        # Send Message
-        slack.post_message("#news", msg)
+            # Result
+            msg = "[{}]\n".format(specific_subject)
+            for keyword, cnt in rank_list:
+                if cnt >= limit:
+                    msg += "{}: {}회 등장\n".format(keyword, cnt)
+                else:
+                    break
+
+            # Send Message
+            slack.post_message("#news", msg)
 
 
 if __name__ == "__main__":
