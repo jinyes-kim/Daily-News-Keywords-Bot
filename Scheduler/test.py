2b552d40009ff0eaa74db90de5a7bb6734283bc3
jinyes-kim <jinung@kakao.com>
2021-09-30 23:54:06 +0900

    의존성 파일 경로 설정 - 2021.09.30.

--- Scheduler/test.py
+++ Scheduler/test.py
@@ -1,27 +1,27 @@
-from airflow.models import DAG
+from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 
 args = {'owner': 'Jinyes', 'start_date': days_ago(n=1)}
 
-dag = DAG(dag_id='Daily-News-Keywords',
+dag = DAG(dag_id='Daily_News_Keywords',
            default_args=args,
            schedule_interval='@daily')
 
 t1 = BashOperator(task_id="crawler",
-                  bash_command="python3 ../Crawler/main.py",
+                  bash_command="python3 /home/jinyes/Daily-News-Keywords-Bot/Crawler/Naver/main.py",
                   dag=dag)
 
 t2 = BashOperator(task_id="producer",
-                  bash_command="python3 ../Producer/producer.py",
+                  bash_command="python3 /home/jinyes/Daily-News-Keywords-Bot/Producer/producer.py",
                   dag=dag)
 
 t3 = BashOperator(task_id="consumer",
-                  bash_command="python3 ../Consumer/main.py",
+                  bash_command="python3  /home/jinyes/Daily-News-Keywords-Bot/Consumer/main.py",
                   dag=dag)
 
 t4 = BashOperator(task_id="slack-bot",
-                  bash_command='python3 ../Bot/main.py',
+                  bash_command='python3 /home/jinyes/Daily-News-Keywords-Bot/Bot/main.py',
                   dag=dag)
 
 t1 >> t2 >> t3 >> t4
