8bd19d11a930b7940e6a90e838a1af5a9789344f
jinyes-kim <jinung@kakao.com>
2021-09-30 15:44:15 +0900

    최대 요청 데이터 건수 수겅 - 2021.09.24.
    [기존]
    디폴트 10건
    
    [변경]
    10,000건

--- Bot/library/es_query.py
+++ Bot/library/es_query.py
@@ -5,13 +5,16 @@ def request_data(date, specific_subject):
     client = Elasticsearch(host="jinyes-server", port=9200)
     result = client.search(
         index="news-test",
+        size=10000,
         query={
             "bool": {
                 "must": [
                     {"match": {"@timestamp": date}},
                     {"match": {"specific_subject": specific_subject}},
                 ]
-            }
+            },
         }
     )
     return result
+
+
