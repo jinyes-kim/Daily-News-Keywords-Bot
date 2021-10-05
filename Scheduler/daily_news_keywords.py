18642a98970d6cd5f3dacf19667b24b9ec25c9aa
jinyes-kim <jinung@kakao.com>
2021-10-05 18:05:13 +0900

    에어플로우 대그 업데이트 - 2021.10.05.

--- Scheduler/daily_news_keywords.py
+++ Scheduler/daily_news_keywords.py
@@ -6,22 +6,26 @@ args = {'owner': 'Jinyes', 'start_date': days_ago(n=1)}
 
 dag = DAG(dag_id='Daily_News_Keywords',
            default_args=args,
-           schedule_interval='@daily')
+           schedule_interval="5 23 * * * ")
 
-t1 = BashOperator(task_id="crawler",
+t1 = BashOperator(task_id="naver_crawler",
                   bash_command="python3 /home/jinyes/Daily-News-Keywords-Bot/Crawler/Naver/main.py",
                   dag=dag)
 
-t2 = BashOperator(task_id="producer",
+t2 = BashOperator(task_id="daum_crawler",
+                  bash_command="python3 /home/jinyes/Daily-News-Keywords-Bot/Crawler/Daum/main.py",
+                  dag=dag)
+
+t3 = BashOperator(task_id="producer",
                   bash_command="python3 /home/jinyes/Daily-News-Keywords-Bot/Producer/producer.py",
                   dag=dag)
 
-t3 = BashOperator(task_id="consumer",
+t4 = BashOperator(task_id="consumer",
                   bash_command="python3  /home/jinyes/Daily-News-Keywords-Bot/Consumer/main.py",
                   dag=dag)
 
-t4 = BashOperator(task_id="slack-bot",
+t5 = BashOperator(task_id="slack-bot",
                   bash_command='python3 /home/jinyes/Daily-News-Keywords-Bot/Bot/main.py',
                   dag=dag)
 
-t1 >> t2 >> t3 >> t4
+[t1, t2] >> t3 >> t4 >> t5
