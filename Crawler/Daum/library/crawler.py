a572311ab808ebc05b379357c1cad81d1a3a3f17
jinyes-kim <jinung@kakao.com>
2021-10-05 17:50:38 +0900

    다음 뉴스 크롤러 개발 - 2021.10.05.

---
+++ Crawler/Daum/library/crawler.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def extract_news(date, category1, category2):
+    records = []
+    for page in range(1, 100):
+        if category2 is not None:
+            url = "https://news.daum.net/breakingnews/{}/{}?page={}&regDate={}".format(category1, category2, page, date)
+        else:
+            url = "https://news.daum.net/breakingnews/{}?page={}&regDate={}".format(category1, page, date)
+
+        request = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+        soup = BeautifulSoup(request.text, "html.parser")
+        target = "#mArticle > div.box_etc > ul > li"
+        news_list = soup.select(target)
+
+        # Stop condition -> last page
+        if not news_list:
+            break
+
+        for news in news_list:
+            record = news.select(".cont_thumb > .tit_thumb > .link_txt")[0]
+            link = record.get("href")
+            title = record.text.strip()
+            records.append([title, link])
+
+    return records
+
+
+
---
+++ Crawler/Daum/main.py
@@ -0,0 +1,45 @@
+from library.crawler import *
+from library.preprocessing import *
+from datetime import datetime
+import json
+import time
+
+# Static variable
+file = json.load(open("info/category.json", 'r'))
+today = datetime.now().strftime("%Y%m%d")
+
+
+def main():
+    records = []
+
+    for keyword in file["keywords"]:
+        category1 = file["keyword_code"][keyword]
+
+        if keyword == "보도자료" or keyword == "자동생성기사":
+            specific_keyword_list = [None]
+        else:
+            specific_keyword_list = file["specific_keywords"][keyword]
+
+        for specific_keyword in specific_keyword_list:
+            category2 = file["specific_keyword_code"][specific_keyword]
+            news_list = extract_news(today, category1, category2)
+
+            for record in news_list:
+                title = record[0]
+                url = record[1]
+                print([keyword, specific_keyword, remove_quot(title), url])
+                records.append([keyword, specific_keyword, remove_quot(title), url])
+
+            time.sleep(1)
+            break
+        break
+
+    with open("../../Data/DAUM{}.txt".format(today), "w") as out:
+        for record in records:
+            raw = ','.join(record)
+            out.write(raw+'\n')
+
+
+if __name__ == "__main__":
+    print("[{}] Start Daum news Crawler".format(datetime.now()))
+    main()
