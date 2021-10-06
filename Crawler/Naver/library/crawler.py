83726cab1549934b6a491313d4bc412a3e0c8659
jinyes-kim <jinung@kakao.com>
2021-10-06 17:27:38 +0900

    코드 리팩토링 - 2021.10.06.
    [기존]
    기존 로직으로는 끝 페이지까지 데이터를 수집하지 못했다.
    html 태그를 가져오는 부분을 하드코딩 하여 불필요한 코드가 존재
    
    [변경]
    페이지를 탐색하면서 현재 페이지의 마지막 뉴스를 캐시로 저장한다. 단 네이버의 경우 헤드라인과 일반 뉴스 두 가지가 존재하므로 cache1, cache2 변수 두 개를 두었다.
    현재 수집한 페이지의 마지막 뉴스가 캐시에 저장한 것과 같다면 마지막 페이지로 간주하고 수집을 중단하는 로직으로 변경

--- Crawler/Naver/library/crawler.py
+++ Crawler/Naver/library/crawler.py
@@ -1,53 +1,47 @@
 from bs4 import BeautifulSoup
-import time
 import requests
-
-"""
-코드 리팩토링 필요함
-뷰티풀 솝 적극 활용하기
-"""
 
 
 def extract_news(date, category1, category2):
-    # 마지막 페이지 탐색
-    url = "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1={}&sid2={}".\
-        format(category1, category2)
-    request = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    soup = BeautifulSoup(request.text, "html.parser")
-    last_page = soup.select(".paging > a")
-    target = (".type06_headline > li > dl > dt:nth-of-type(1) > a",
-              ".type06 > li > dl > dt:nth-of-type(1) > a")
+    cache1, cache2 = None, None
+    records = []
 
-    # 섹션별 뉴스 데이터 수집
-    result = []
-    for i in range(1, len(last_page)+2):
-        url = "https://news.naver.com/main/list.naver?mode=LS2D&sid2={1}&sid1={0}&mid=shm&date={2}&page={3}".\
+    for i in range(1, 100):
+        url = "https://news.naver.com/main/list.naver?mode=LS2D&sid2={1}&sid1={0}&mid=shm&date={2}&page={3}". \
             format(category1, category2, date, i)
         request = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
         soup = BeautifulSoup(request.content.decode("euc-kr", "replace"), "html.parser")
 
-        for section in target:
-            result.extend(soup.select(section))
-        time.sleep(2)
+        target1 = "#main_content > div.list_body.newsflash_body > ul.type06_headline > li > dl > dt > a"
+        target2 = "#main_content > div.list_body.newsflash_body > ul.type06 > li > dl > dt > a"
 
-    return result
+        merged_list = []
+        news1_list = soup.select(target1)
+        news2_list = soup.select(target2)
+        merged_list.extend(news1_list)
+        merged_list.extend(news2_list)
 
+        # Stop condition1
+        try:
+            if cache1 == news1_list[-1].text.strip():
+                break
+            cache1 = news1_list[-1].text.strip()
+        except:
+            pass
 
-def extract_news_title(raw_data):
-    result = []
-    for line in raw_data:
-        line = str(line).replace("\n", '')
-        if "<img" in line:
-            dummy = line.split('">')
-            news_url = dummy[0].replace('<a href="', '')
-            news_title = dummy[1].split(" height")[0].replace("<img alt=", '')
-        else:
-            dummy = line.split("</a>")[0].split('">')
-            news_url = dummy[0].replace('<a href="', '')
-            news_title = dummy[1]
+        # Stop condition2
+        try:
+            if cache2 == news2_list[-1].text.strip():
+                break
+            cache2 = news2_list[-1].text.strip()
+        except:
+            pass
 
-        if news_title != '':
-            result.append([news_title.strip(), news_url.strip()])
-    return result
+        for raw in merged_list:
+            title = raw.text.strip()
+            url = raw.get("href")
+            records.append([title, url])
+
+    return records
 
 
